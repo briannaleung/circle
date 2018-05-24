30fd1c076d34434c77a10baad5c81d1eecc606c5
Brianna leung <b27leung@edu.uwaterloo.ca>
2018-05-23 21:30:14 -0400

    used pixi.js for smoother circle

--- script.js
+++ script.js
@@ -1,141 +1,160 @@
-var circle;
-var context;
-var cancan;
-var movement = 20;
-var size = 5;
-
-document.addEventListener('keydown', function(event) {
-	if(event.keyCode == 87){
-		circle.moveUp();
-	} else if (event.keyCode == 83){
-		circle.moveDown();
-	} else if (event.keyCode == 65){
-		circle.moveLeft();
-	} else if (event.keyCode == 68){
-		circle.moveRight();
-	} else if (event.keyCode == 84){
-		circle.changeColor();
-	} else if (event.keyCode == 79){
-        circle.increaseSize();
-    } else if (event.keyCode == 80){
-        circle.decreaseSize();
-    } else if (event.keyCode == 85){
-        circle.increaseThickness();
-    } else if (event.keyCode == 73){
-        circle.decreaseThickness();
-    } else if (event.keyCode == 82){
-        circle.reset();
+//Aliases
+let Application = PIXI.Application,
+    Container = PIXI.Container,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    TextureCache = PIXI.utils.TextureCache,
+    Sprite = PIXI.Sprite,
+    Text = PIXI.Text,
+    TextStyle = PIXI.TextStyle;
+//Create a Pixi Application
+let app = new Application({ 
+    width: 2560, 
+    height: 2560,                       
+    antialiasing: true, 
+    transparent: false, 
+    resolution: 1,
+    backgroundColor: 0x000000
+  }
+);
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
+loader
+  .add("circleicon.png")
+  .load(setup);
+//Define any variables that are used in more than one function
+var cat, state;
+function setup() {
+  //Create the `cat` sprite 
+  cat = new Sprite(resources["circleicon.png"].texture);
+  cat.x = 96;
+  cat.y = 96; 
+  cat.vx = 0;
+  cat.vy = 0;
+  cat.width = 120;
+  cat.height = 120;
+  app.stage.addChild(cat);
+  //Capture the keyboard arrow keys
+  let left = keyboard(37),
+      up = keyboard(38),
+      right = keyboard(39),
+      down = keyboard(40);
+  //Left arrow key `press` method
+  left.press = () => {
+    //Change the cat's velocity when the key is pressed
+    cat.vx = -5;
+    cat.vy = 0;
+  };
+  
+  //Left arrow key `release` method
+  left.release = () => {
+    //If the left arrow has been released, and the right arrow isn't down,
+    //and the cat isn't moving vertically:
+    //Stop the cat
+    if (!right.isDown && cat.vy === 0) {
+      cat.vx = 0;
     }
-});
-
-window.onload = function() {
-    cancan = document.getElementById('cancan');
-    context = cancan.getContext('2d');
-    circle = new Circle();
+  };
+  //Up
+  up.press = () => {
+    cat.vy = -5;
+    cat.vx = 0;
+  };
+  up.release = () => {
+    if (!down.isDown && cat.vx === 0) {
+      cat.vy = 0;
+    }
+  };
+  //Right
+  right.press = () => {
+    cat.vx = 5;
+    cat.vy = 0;
+  };
+  right.release = () => {
+    if (!left.isDown && cat.vy === 0) {
+      cat.vx = 0;
+    }
+  };
+  //Down
+  down.press = () => {
+    cat.vy = 5;
+    cat.vx = 0;
+  };
+  down.release = () => {
+    if (!up.isDown && cat.vx === 0) {
+      cat.vy = 0;
+    }
+  };
+  //Set the game state
+  state = play;
+ 
+  //Start the game loop 
+  app.ticker.add(delta => gameLoop(delta));
 }
-
-class Circle {
-	constructor() {
-        this.x = 700;
-        this.y = 600;
-        this.r = 50;
-        this.start = 0;
-        this.end = Math.PI*2;
-        this.color = "#FF0000";
-        this.lineWidth="5";
-        this.draw();     
+function gameLoop(delta){
+  //Update the current game state:
+  state(delta);
+}
+function play(delta) {
+  //Use the cat's velocity to make it move
+  cat.x += cat.vx;
+  cat.y += cat.vy
+}
+//The `keyboard` helper function
+function keyboard(keyCode) {
+  var key = {};
+  key.code = keyCode;
+  key.isDown = false;
+  key.isUp = true;
+  key.press = undefined;
+  key.release = undefined;
+  //The `downHandler`
+  key.downHandler = event => {
+    if (event.keyCode === key.code) {
+      if (key.isUp && key.press) key.press();
+      key.isDown = true;
+      key.isUp = false;
     }
-
-    draw() {
-        context.strokeStyle = this.color;
-        context.lineWidth=this.lineWidth;
-        context.beginPath();
-        context.arc(this.x, this.y, this.r, this.start, this.end);
-        context.stroke();
+    event.preventDefault();
+  };
+  //The `upHandler`
+  key.upHandler = event => {
+    if (event.keyCode === key.code) {
+      if (key.isDown && key.release) key.release();
+      key.isDown = false;
+      key.isUp = true;
     }
-    
-    erase() { 
-        context.clearRect(0, 0, cancan.width, cancan.height);
-    }
+    event.preventDefault();
+  };
+  //Attach event listeners
+  window.addEventListener(
+    "keydown", key.downHandler.bind(key), false
+  );
+  window.addEventListener(
+    "keyup", key.upHandler.bind(key), false
+  );
 
-    getRandomColor() {
-	  	var letters = '0123456789ABCDEF';
-	  	var color = '#';
-	  	for (var i = 0; i < 6; i++) {
-	    	color += letters[Math.floor(Math.random() * 16)];
-	  	}
-	  	return color;
-	}
-
-    moveUp() {
-        this.erase();
-        this.y = this.y - movement;
-        this.draw();
-    }
-
-    moveDown() {
-    	this.erase();
-        this.y = this.y + movement;
-        this.draw();
-    }
-
-    moveLeft() {
-    	this.erase();
-        this.x = this.x - movement;
-        this.draw();
-    }
-
-    moveRight() {
-    	this.erase();
-        this.x = this.x + movement;
-        this.draw();
-    }
-
-	changeColor() {
-    	this.erase();
-
-    	var letters = '0123456789ABCDEF';
-	  	var color = '#';
-	  	for (var i = 0; i < 6; i++) {
-	    	color += letters[Math.floor(Math.random() * 16)];
-	  	}
-
-	  	this.color = color;
-    	this.draw();
-    }
-
-    increaseSize() {
-        this.erase();
-        this.r = this.r + size;
-        this.draw();
-    }
-
-    decreaseSize() {
-        this.erase();
-        this.r = this.r - size;
-        this.draw();
-    }
-
-    increaseThickness() {
-        this.erase();
-        this.lineWidth++;
-        this.draw();
-    }
-
-    decreaseThickness() {
-        this.erase();
-        this.lineWidth--;
-        this.draw();
-    }
-
-    reset() {
-        this.erase();
-        this.x = 700;
-        this.y = 600;
-        this.r = 50;
-        this.start = 0;
-        this.color = "#FF0000";
-        this.lineWidth="5";
-        this.draw();
-    }
+  //1. Simple text
+let message = new Text("Hello Pixi!");
+//Position it and add it to the stage
+message.position.set(54, 96);
+//app.stage.addChild(message);
+//2. Styled text
+let style = new TextStyle({
+  fontFamily: "Arial",
+  fontSize: 36,
+  fill: "white",
+  stroke: '#ff3300',
+  strokeThickness: 4,
+  dropShadow: true,
+  dropShadowColor: "#000000",
+  dropShadowBlur: 4,
+  dropShadowAngle: Math.PI / 6,
+  dropShadowDistance: 6,
+});
+let styledMessage = new Text("Beat that WenYu", style);
+//Position it and add it to the stage
+styledMessage.position.set(130, 128);
+//app.stage.addChild(styledMessage);
+  return key;
 }
